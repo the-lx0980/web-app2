f6b1d0dd23e0b663d2077bb5dd2ad2301c15f799
Vishal1main <monsterr258258@gmail.com>
2024-09-11 20:01:53 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -10,7 +10,7 @@ class Telegram:
     BOT_TOKEN = getenv("BOT_TOKEN", "6737943130:AAENzY3vZdDVo6jsfvvip019IRzbp38h01U")
     PORT = int(getenv("PORT", 8080))
     SESSION_STRING = getenv("SESSION_STRING", "BQGYpOMAR8CnerlWSNx_h66UEswxIZDoY_-SZ6BpOOgfAMshAypvLsIPFtLhCqNNyxane7ErDJ6xsAm6ELzwZ2pJ41v4QBesjVBJxkAGTIvLdqMg8f6IfZEUZi_1gftJWBjBCCy4ep-71hYX_BUIcV6YL0ZQ6arz4LjhdFsY6GNgpEAPmT1C1fgxIeUSGFdfCJV-ZdwOFvNN1JvLz_wgv3mTjjhzhziwm5RsyWUJlLYcj35_kSLIdOfJWb9Y8UeJGSAGkMU499Zk9LcL89XtvLvvsd5voesSFYrczvaJmtOLkmbBWouWaYbGmzPIJxpl2QVrzKtCbWJbOSyrg6452PpIgcIUowAAAAGpf-0jAA")
-    BASE_URL = getenv("BASE_URL", "https://interim-sheelagh-rrrrr-88421628.koyeb.app/").rstrip('/')
+    BASE_URL = getenv("BASE_URL", "https://interim-sheelagh-rrrrr-88421628.koyeb.app/").rstrip('https://interim-sheelagh-rrrrr-88421628.koyeb.app/')
     DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://tabolo8539:0evqZDV4fC5fD17c@cluster0.cw8zxus.mongodb.net/?retryWrites=true&w=majority")
     AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "-1002248276059").split(",")]
     THEME = getenv("THEME", "quartz").lower()
