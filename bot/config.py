537c0502cd7cac4c342be1011e3e3cbf8aad0156
Vishal1main <monsterr258258@gmail.com>
2024-09-11 19:34:20 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,14 +5,14 @@ load_dotenv("config.env")
 
 
 class Telegram:
-    API_ID = int(getenv("API_ID", "0"))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "26780899"))
+    API_HASH = getenv("API_HASH", "88c1b5a2a369695990bfce2ecd4fe601")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6737943130:AAENzY3vZdDVo6jsfvvip019IRzbp38h01U")
     PORT = int(getenv("PORT", 8080))
-    SESSION_STRING = getenv("SESSION_STRING", "")
+    SESSION_STRING = getenv("SESSION_STRING", "BQGYpOMAR8CnerlWSNx_h66UEswxIZDoY_-SZ6BpOOgfAMshAypvLsIPFtLhCqNNyxane7ErDJ6xsAm6ELzwZ2pJ41v4QBesjVBJxkAGTIvLdqMg8f6IfZEUZi_1gftJWBjBCCy4ep-71hYX_BUIcV6YL0ZQ6arz4LjhdFsY6GNgpEAPmT1C1fgxIeUSGFdfCJV-ZdwOFvNN1JvLz_wgv3mTjjhzhziwm5RsyWUJlLYcj35_kSLIdOfJWb9Y8UeJGSAGkMU499Zk9LcL89XtvLvvsd5voesSFYrczvaJmtOLkmbBWouWaYbGmzPIJxpl2QVrzKtCbWJbOSyrg6452PpIgcIUowAAAAGpf-0jAA")
     BASE_URL = getenv("BASE_URL").rstrip('/')
-    DATABASE_URL = getenv("DATABASE_URL")
-    AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL").split(",")]
+    DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://tabolo8539:0evqZDV4fC5fD17c@cluster0.cw8zxus.mongodb.net/?retryWrites=true&w=majority")
+    AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL", "-1002248276059").split(",")]
     THEME = getenv("THEME", "quartz").lower()
     USERNAME = getenv("USERNAME", "admin")
     PASSWORD = getenv("PASSWORD", "admin")
